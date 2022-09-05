23ea45d08ebd21c7bc52b8e1dda29a2a49718bc0
freddy.liu <freddy.liu@positivegrid.com>
2022-09-05 11:17:41 +0800

    feat: add shopify hydrogen framework notes

--- src/assets/blogContent/blogPosts/index.tsx
+++ src/assets/blogContent/blogPosts/index.tsx
@@ -1,4 +1,5 @@
 import { ReactElement } from 'react';
+import ShopifyHydrogen from './shopify/shopify-hydrogen.mdx';
 import RustBeginners from './rust/rust-beginners.mdx';
 import RustIntermediate from './rust/rust-intermediate.mdx';
 
@@ -7,6 +8,7 @@ type BlogPostsType = {
 };
 
 const blogPosts: BlogPostsType = {
+  'shopify-hydrogen': <ShopifyHydrogen />,
   'rust-beginners': <RustBeginners />,
   'rust-intermediate': <RustIntermediate />,
 };
